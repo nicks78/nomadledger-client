2a4ebfe7d3e2a2db894cddae22ea7c7cb11a5f04
Nicolas <contact@apx-dev.com>
2019-07-21 05:42:23 +0200

    fix mobileview

--- src/pages/auth/components/contact.js
+++ src/pages/auth/components/contact.js
@@ -1,7 +1,7 @@
 //src/pages/auth/components/appBar.js
 import React from 'react'
 import {DEFAULT_URL} from '../../../redux/constant'
-import {withStyles, Typography} from '@material-ui/core'
+import {withStyles, Typography, Grid} from '@material-ui/core'
 
 const  AppBar = (props) => {
 
@@ -9,19 +9,26 @@ const  AppBar = (props) => {
 
     return (
         <div className={classes.root} >
-            <div>
+            <Grid container spacing={24}>
+
+                <Grid item xs={12} md={4} sm={4}>
                 <img src={`${DEFAULT_URL}img/${isMobile ? "logo" : "logo-full" }.png`} alt="logo-full" width={isMobile ? "80" : "246"} />
-            </div>
-            <div>
+                </Grid>
+
+                <Grid item xs={12} md={4} sm={4}>
                 <Typography variant="body1">{locale.home_page.contact.contact_us}</Typography>
-                <a href="mailto:contact@nomadledger.com"  className="link" style={{fontSize: "0.8rem"}}>contact@nomadledger.com</a>
-            </div>
-            <div>
+                <a href="mailto:contact@nomadledger.com" className="link" style={{fontSize: "0.8rem"}}>contact@nomadledger.com</a>
+                </Grid>
+
+                <Grid item xs={12} md={4} sm={4}>
                 <Typography variant="body1">{locale.home_page.contact.follow_us}</Typography>
                 <div className={ classes.socialContainer }>
                     <a target="_blank" href="https://www.facebook.com/NomadLedger-2385240291708965" rel="noopener noreferrer"><img src={`${DEFAULT_URL}img/facebook-512.png`} alt="logo-facebook" width="15" /></a>
                 </div>
-            </div>
+                </Grid>
+
+            </Grid>
+          
         </div>
     )
 }
@@ -29,14 +36,7 @@ const  AppBar = (props) => {
 const styles = theme => ({
 
     root: {
-      display: "flex",
-      justifyContent: "space-between",
-      marginLeft: 200,
-      marginRight: 200,
-      [theme.breakpoints.down('md')]:{
-        marginLeft: 0,
-        marginRight: 0,
-      }
+        textAlign: "center"
     },
     socialContainer: {
         marginTop: 5
