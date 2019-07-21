5411225e327806188c0bcc909cbc74019ec35b56
Nicolas <contact@apx-dev.com>
2019-07-21 15:24:18 +0200

    add delete cache

--- src/CacheBuster.js
+++ src/CacheBuster.js
@@ -31,12 +31,12 @@ class CacheBuster extends React.Component {
       reloadedAt: null,
       refreshCacheAndReload: () => {
         console.log('Clearing cache and hard reloading...')
-        // if (caches) {
-        //   // Service worker cache should be cleared with caches.delete()
-        //   caches.keys().then(function(names) {
-        //     for (let name of names) caches.delete(name);
-        //   });
-        // }
+        if (caches) {
+          // Service worker cache should be cleared with caches.delete()
+          caches.keys().then(function(names) {
+            for (let name of names) caches.delete(name);
+          });
+        }
 
         // delete browser cache and hard reload
         window.location.reload(true);
