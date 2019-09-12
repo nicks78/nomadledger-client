18d08bd8fc352ebc789aaba73e65b19955f7340a
Nicolas <contact@apx-dev.com>
2019-09-12 02:02:32 +0200

    remove log

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,17 +8,18 @@ import { history } from '../routes/history'
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-const logger = (store) => (next) => (action) => {
-  if (typeof action !== "function") {
-    console.log("DISPATCH", action)
-  }
-  return next(action);
-}
+// const logger = (store) => (next) => (action) => {
+//   if (typeof action !== "function") {
+//     console.log("DISPATCH", action)
+//   }
+//   return next(action);
+// }
+
 
 const store = createStore(reducers,
   compose(
     applyMiddleware(logger, thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   ));
 
 store.dispatch(getHelpers());
